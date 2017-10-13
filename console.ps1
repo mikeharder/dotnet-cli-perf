671d291e61d9ced8ccc2a4683e32a1b2cdcec032
Mike Harder <mharder@microsoft.com>
2017-10-13 22:45:56 +0000

    Remove unnecessary command

--- console.ps1
+++ console.ps1
@@ -16,9 +16,6 @@ Set-Location console
 
 Measure-Command-With-Output { dotnet new console --no-restore }
 
-# Terminate app after web server is started
-(Get-Content ./Program.cs).Replace('Run()', 'RunAsync()') | Set-Content ./Program.cs
-
 Measure-Command-With-Output { dotnet restore }
 
 Measure-Command-With-Output { dotnet restore }
