0e2d0f16110219c401eb7a4764e3af9277b3efb4
Mike Harder <mharder@microsoft.com>
2017-10-18 11:51:48 -0700

    Add warmupCount command line arg

--- DotNetCliPerf/Program.cs
+++ DotNetCliPerf/Program.cs
@@ -9,12 +9,13 @@ namespace DotNetCliPerf
     {
         static void Main(string[] args)
         {
-            var targetCount = args.Length >= 1 ? int.Parse(args[0]) : 1;
+            var warmupCount = args.Length >= 1 ? int.Parse(args[0]) : 1;
+            var targetCount = args.Length >= 2 ? int.Parse(args[1]) : 1;
 
             var job = new Job();
             job.Run.RunStrategy = RunStrategy.Monitoring;
             job.Run.LaunchCount = 1;
-            job.Run.WarmupCount = 1;
+            job.Run.WarmupCount = warmupCount;
             job.Run.TargetCount = targetCount;
 
             var config = ManualConfig.Create(DefaultConfig.Instance).With(job);
