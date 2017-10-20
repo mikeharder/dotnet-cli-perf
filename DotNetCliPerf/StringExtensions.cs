fdc1271a2b5e09c33f52847270e982f84e3403fd
Mike Harder <mharder@microsoft.com>
2017-10-20 10:13:23 -0700

    Fix ContainsAny to match start of string

--- DotNetCliPerf/StringExtensions.cs
+++ DotNetCliPerf/StringExtensions.cs
@@ -9,7 +9,7 @@ namespace DotNetCliPerf
         public static bool ContainsAny(this string input, IEnumerable<string> patterns,
             StringComparison comparisonType = StringComparison.Ordinal)
         {
-            return patterns.Any(p => input.IndexOf(p, comparisonType) > 0);
+            return patterns.Any(p => input.IndexOf(p, comparisonType) >= 0);
         }
     }
 }
