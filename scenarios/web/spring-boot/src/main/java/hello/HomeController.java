024ff5d9c1258825a12728036441fd64604c094e
Ryan Nowak <nowakra@gmail.com>
2017-11-02 16:01:45 -0700

    Add spring boot version of the MVC template (#1)
    Based on https://spring.io/guides/gs/serving-web-content/ and updated to
    match the MVC template.
    
    To run:
    
    ./gradlew bootRun

---
+++ scenarios/web/spring-boot/src/main/java/hello/HomeController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+    		model.addAttribute("title", "temp");
+        return "index";
+    }
+
+}
