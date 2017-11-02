6836919f2f51b1e752e31b685bad337d7c5670fe
Mike Harder <mharder@microsoft.com>
2017-11-02 15:00:53 -0700

    Execute request after starting app

--- scenarios/web/core/Program.cs
+++ scenarios/web/core/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -14,7 +16,13 @@ namespace core
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var sw = Stopwatch.StartNew();
+            BuildWebHost(args).RunAsync();
+            var response = (new HttpClient()).GetStringAsync("http://localhost:5000").Result;
+            sw.Stop();
+
+            Console.WriteLine(response);
+            Console.WriteLine(sw.Elapsed);
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
