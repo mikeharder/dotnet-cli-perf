7dd87cefba6bb3eb2abf34e1340d8a3e8f711fed
Mike Harder <mharder@microsoft.com>
2017-11-13 21:39:49 -0800

    Code cleanup

--- DotNetCliPerf/WebAppFramework.cs
+++ DotNetCliPerf/WebAppFramework.cs
@@ -10,6 +10,8 @@ namespace DotNetCliPerf
 {
     public class WebAppFramework : FrameworkApp
     {
+        private (Process Process, StringBuilder OutputBuilder, StringBuilder ErrorBuilder) _process;
+
         protected override string SourceDir => Path.Combine("web", "framework");
 
         protected override string SourcePath => Path.Combine(RootTempDir, "mvc", "Controllers", "HomeController.cs");
@@ -24,20 +26,20 @@ namespace DotNetCliPerf
                 Path.Combine("mvc", "obj"),
             });
 
-        private (Process Process, StringBuilder OutputBuilder, StringBuilder ErrorBuilder) _process;
         protected override string Run(bool first = false)
         {
             Build(first);
-            _process = IISExpress("mvc");
+            _process = StartIISExpress("mvc");
             return HttpClient.GetStringAsync("http://localhost:5000").Result;
         }
 
         protected override void RunCleanup()
         {
+            base.RunCleanup();
             Util.StopProcess(_process.Process, _process.OutputBuilder, _process.ErrorBuilder);
         }
 
-        protected (Process Process, StringBuilder OutputBuilder, StringBuilder ErrorBuilder) IISExpress(string relativePath, int port = 5000, bool systray = false)
+        protected (Process Process, StringBuilder OutputBuilder, StringBuilder ErrorBuilder) StartIISExpress(string relativePath, int port = 5000, bool systray = false)
         {
             var path = Path.Combine(RootTempDir, relativePath);
             return Util.StartProcess("iisexpress", $"/path:{path} /port:{port} /systray:{systray}", RootTempDir);
