6a7a7a1a86efe40191be24a51587a54c026fad9e
Mike Harder <mharder@microsoft.com>
2017-12-05 16:29:00 -0800

    Use "nuget.exe restore" for Framework apps

--- DotNetCliPerf/FrameworkApp.cs
+++ DotNetCliPerf/FrameworkApp.cs
@@ -16,7 +16,20 @@ namespace DotNetCliPerf
 
         protected override void Build(bool first = false)
         {
-            MSBuild("/t:build", restore: first || Restore);
+            if (first || Restore)
+            {
+                // For Framework apps, restore via "nuget.exe restore" rather than "msbuild.exe /restore", for 2 reasons:
+                // 1. Framework apps often use packages.config rather than PackageReference, which is only supported by nuget.exe
+                // 2. Customers are more likely to use "nuget.exe restore" since it has existed for a lot longer
+                NuGet("restore");
+            }
+
+            MSBuild("/t:build");
+        }
+
+        private void NuGet(string arguments)
+        {
+            Util.RunProcess("nuget", arguments, RootTempDir);
         }
 
         protected string VsTestConsole(string arguments, bool throwOnError = true)
