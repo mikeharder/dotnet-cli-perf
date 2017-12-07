0eeec17bc835161ee8e7a23846e74506bf44a1c5
Mike Harder <mharder@microsoft.com>
2017-12-07 13:46:30 -0800

    Improve perf of DirectoryCopy

--- Common/Util.cs
+++ Common/Util.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Common
 {
@@ -199,7 +200,6 @@ namespace Common
                     + sourceDirName);
             }
 
-            DirectoryInfo[] dirs = dir.GetDirectories();
             // If the destination directory doesn't exist, create it.
             if (!Directory.Exists(destDirName))
             {
@@ -207,21 +207,20 @@ namespace Common
             }
 
             // Get the files in the directory and copy them to the new location.
-            FileInfo[] files = dir.GetFiles();
-            foreach (FileInfo file in files)
+            Parallel.ForEach(dir.EnumerateFiles(), file =>
             {
                 string temppath = Path.Combine(destDirName, file.Name);
                 file.CopyTo(temppath, false);
-            }
+            });
 
             // If copying subdirectories, copy them and their contents to new location.
             if (copySubDirs)
             {
-                foreach (DirectoryInfo subdir in dirs)
+                Parallel.ForEach(dir.EnumerateDirectories(), subdir =>
                 {
                     string temppath = Path.Combine(destDirName, subdir.Name);
                     DirectoryCopy(subdir.FullName, temppath, copySubDirs);
-                }
+                });
             }
         }
     }
