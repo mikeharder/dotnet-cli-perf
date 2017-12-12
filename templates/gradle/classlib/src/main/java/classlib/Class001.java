3fb7220da7a72975e229653862850176bd553aac
Mike Harder <mharder@microsoft.com>
2017-12-11 23:00:18 -0800

    WIP: Gradle Templates

---
+++ templates/gradle/classlib/src/main/java/classlib/Class001.java
@@ -0,0 +1,7 @@
+package classlib;
+
+public class Class001 {
+    public static string property() {
+        return "classlib";
+    }
+}
---
+++ templates/gradle/mvc/src/main/java/hello/Application.java
@@ -0,0 +1,45 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StopWatch;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+		
+		if (args.length >= 1 && args[0].equalsIgnoreCase("--mode=singleRequest")) {
+			StopWatch stopwatch = new StopWatch("dotnet-cli-perf");
+    		
+			stopwatch.start("App Startup");
+			ApplicationContext context = SpringApplication.run(Application.class, args);
+			stopwatch.stop();
+			
+			stopwatch.start("First Request");
+			RestTemplate template = new RestTemplate();
+			ResponseEntity<String> response = template.getForEntity("http://localhost:8080", String.class);
+			stopwatch.stop();
+
+			System.out.println(response.getBody());
+			if (response.getStatusCodeValue() != 200) {
+				throw new RuntimeException("Response was: " + response.getStatusCodeValue());
+			}
+
+			System.out.println();
+			System.out.println("Total time to request: " + stopwatch.getTotalTimeMillis() + "ms");
+			System.out.println();
+			System.out.println();
+			System.out.print(stopwatch.prettyPrint());
+			System.out.println();
+			
+			System.exit(0);
+		}
+		else {
+			SpringApplication.run(Application.class, args);
+		}
+    }
+}
