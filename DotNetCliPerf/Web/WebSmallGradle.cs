6621bfeb53dfb45f6218587e8984e5ff7a889cc9
Mike Harder <mharder@microsoft.com>
2017-12-21 17:33:21 -0800

    Add comment

--- DotNetCliPerf/Web/WebSmallGradle.cs
+++ DotNetCliPerf/Web/WebSmallGradle.cs
@@ -11,6 +11,7 @@ namespace DotNetCliPerf
 
         protected override string ExpectedOutput => $"<title>{NewValue}";
 
+        // We still need to use the "singleRequest" model for Gradle, since killing the process also kills the Gradle daemon.
         protected override string Run(bool first = false)
         {
             return GradleW("bootRun -PappArgs=\"['--mode=singleRequest']\"");
