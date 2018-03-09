22b65d989cf23fb81b99c6f283e6595a724a0e48
Mike Harder <mharder@microsoft.com>
2018-03-08 18:59:46 -0800

    Add Kestrel and Mvc as scenarios

---
+++ DotNetCliPerf/RealWorld/Mvc.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetCliPerf
+{
+    public class Mvc : BuildOnlyCoreApp
+    {
+        protected override string SourceDir => "Mvc";
+
+        public override string Solution => "Mvc.sln";
+    }
+}
