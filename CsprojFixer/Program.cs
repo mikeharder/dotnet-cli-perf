a463a5dd8db10eb2979c40289e3ed4eaf133ce37
Mike Harder <mharder@microsoft.com>
2018-03-12 10:44:08 -0700

    Enumerate recursive csproj files

---
+++ CsprojFixer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CsprojFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var projects = GetProjects(args[0]);
+            foreach (var project in projects)
+            {
+                Console.WriteLine(project);
+            }
+        }
+
+        private static IEnumerable<string> GetProjects(string path)
+        {
+            foreach (var file in Directory.EnumerateFiles(path))
+            {
+                if (Path.GetExtension(file) == ".csproj")
+                {
+                    yield return file;
+                }
+            }
+
+            foreach (var directory in Directory.EnumerateDirectories(path))
+            {
+                foreach (var file in GetProjects(directory))
+                {
+                    yield return file;
+                }
+            }
+        }
+    }
+}
