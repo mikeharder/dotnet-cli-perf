220bc0412920b874903ca3868fc70a3e07d9d4ff
Mike Harder <mharder@microsoft.com>
2018-08-16 17:53:48 -0700

    Add SdkVersion 2.1.402

--- DotNetCliPerf/CoreApp.cs
+++ DotNetCliPerf/CoreApp.cs
@@ -12,7 +12,7 @@ namespace DotNetCliPerf
     {
         private const string _globalJson = @"{ ""sdk"": { ""version"": ""0.0.0"" } }";
 
-        [Params("2.1.400", "2.1.401", "2.2.100-preview1")]
+        [Params("2.1.400", "2.1.401", "2.1.402", "2.2.100-preview1")]
         public string SdkVersion { get; set; }
 
         [Params(true, false)]
